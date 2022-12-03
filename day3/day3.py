e981aed389b8ac7edfc84245b3e51d6431c56060
Ludvig Gierup <lgierup25@gmail.com>
2022-12-03 13:00:12 +0100

    Cleaned up solution for day 3

--- day3/day3.py
+++ day3/day3.py
@@ -3,34 +3,26 @@ def priority(char):
         return ord(char) - 38
     return ord(char) - 96
 
-def group(xs):
-    return list(xs[i:i+3] for i in range(0, len(xs), 3))
-
 with open('input') as file:
     lines = file.read().strip().split('\n')
     # Part 1 
-    errors = []
+    score1 = 0
     for line in lines:
         first = line[:len(line)//2]
         second = line[len(line)//2:]
-
-        found = False
         for char in first:
-            if found:
-                continue
             if char in second:
-                errors.append(char)
-                found = True
+                score1 += priority(char)
+                break
+    print(score1)
 
     # Part 2
-    badges = []
-    for elfgroup in group(lines):
-        print(group(lines))
-        badge = list(set(elfgroup[0]) & set(elfgroup[1]) & set(elfgroup[2])).pop()
-        badges.append(badge)
-
-    part1 = sum(list(map(priority, errors)))
-    print(part1)
-    
-    part2 = sum(list(map(priority, badges)))
-    print(part2)
+    score2 = 0
+    i = 0
+    while i < len(lines):
+        for c in lines[i]:
+            if c in lines[i+1] and c in lines[i+2]:
+                score2 += priority(c)
+                break
+        i += 3
+    print(score2)
